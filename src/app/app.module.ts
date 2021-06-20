422ece16e947fc48e62b359dff398f6a811a8099
Vedseroz <50847828+Vedseroz@users.noreply.github.com>
2021-06-19 20:25:05 -0400

    agregados los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { CategoriasComponent } from './categorias/categorias.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    CategoriasComponent,
+    CarritoComponent,
+    FormularioComponent,
+    UsuarioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
