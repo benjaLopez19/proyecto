39cd30bf795198c02f8ec3e5d7aa04aeff8ef5bf
benjaLopez19 <82004309+benjaLopez19@users.noreply.github.com>
2021-06-20 23:24:40 -0400

    formControl

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,32 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { Router } from '@angular/router';
+import {FormGroup,FormBuilder, Validators, AbstractControl, FormControl} from '@angular/forms';
 
-=======
-import {FormGroup,FormBuilder, Validators, AbstractControl} from '@angular/forms';
->>>>>>> 6faa3781ba2356c0d6f46082f6252629ae9c2d19
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  busqueda:FormGroup;
-  auxiliar:AbstractControl;
+ 
+  search = new FormControl('');
 
   constructor(public fb:FormBuilder) { 
-    this.busqueda=this.fb.group(
-      {barra:['',[Validators.required]] }
-    );
-    this.auxiliar = this.busqueda.controls['search'];
   }
 
   ngOnInit(): void {
   }
 
-  buscar(){
-    let b1=this.busqueda.get('search')?.value;
-    console.log(b1);
+  searchProduct(){
+    console.log(this.search.value);
   }
 }
